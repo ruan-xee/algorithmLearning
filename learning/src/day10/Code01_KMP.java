fa82b48461d7cb9cfd08014fbd6e44fb91669b1a
ruanzh <ruanzh@yinhai.com>
2023-03-24 10:53:23 +0800

    KMP算法

---
+++ learning/src/day10/Code01_KMP.java
@@ -0,0 +1,53 @@
+package day10;
+
+public class Code01_KMP {
+    public static int getIndexOf(String s, String m) {
+        if (s == null || m == null || m.length() < 1 || s.length() < m.length()) {
+            return -1;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = m.toCharArray();
+        int i1 = 0;
+        int i2 = 0;
+        int[] next = getNextArray(str2);
+        while (i1 < s.length() && i2 < m.length()) {
+            if (str1[i1] == str2[i2]) {
+                i1++;
+                i2++;
+            } else if (next[i2] == -1) {
+                i1++;
+            } else {
+                i2 = next[i2];
+            }
+        }
+        return i2 == str2.length ? i1 - i2 : -1;
+    }
+
+    public static int[] getNextArray(char[] str) {
+        if (str.length == 1) {
+            return new int[]{-1};
+        }
+        int[] next = new int[str.length];
+        next[0] = -1;
+        next[1] = 0;
+        int i = 2;
+        int cn = 0;
+        while (i < next.length) {
+            if (str[i - 1] == str[cn]) {
+                next[i++] = ++cn;
+            } else if (cn > 0) {
+                cn = next[cn];
+            } else {
+                next[i++] = 0;
+            }
+        }
+        return next;
+    }
+
+
+    public static void main(String[] args) {
+        String str1 = "ababccdtababcc";
+        String str2 = "tababc";
+        System.out.println(getIndexOf(str1,str2));
+    }
+}
